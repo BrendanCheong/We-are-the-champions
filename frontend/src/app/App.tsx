35103571902b3b0c8c5fd85cf3c3d95fd34cc06c
Brendan Cheong <brendancej1@gmail.com>
2024-09-19 11:49:58 +0800

    [FEAT-FRONTEND]: create skeleton demo of single page app
    - We will use this page as the main structure of our app

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,47 +1,139 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { useHelloQuery } from "../features/hello/api/useHelloQuery";
+import useIsMobile from "@/hooks/useIsMobile";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Textarea } from "@/components/ui/textarea";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import TableDemo from "@/components/TableDemo";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const { data, isLoading, isError, error } = useHelloQuery();
+  const isMobile = useIsMobile();
 
   return (
-    <>
-      <div>
-        <a
-          className="flex flex-col justify-center"
-          href="https://vitejs.dev"
-          target="_blank"
-        >
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a
-          className="flex flex-col justify-center"
-          href="https://react.dev"
-          target="_blank"
-        >
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className={`${isMobile ? "flex flex-col" : "flex h-screen"}`}>
+      {!isMobile && (
+        // Quick Actions Bar (Desktop only)
+        <div className="w-[4%] bg-gray-100 dark:bg-gray-800 p-2 flex flex-col items-center">
+          <div className="mb-4 cursor-pointer">Icon 1</div>
+          <div className="mb-4 cursor-pointer">Icon 2</div>
+          <div className="mb-4 cursor-pointer">Icon 3</div>
+        </div>
+      )}
+
+      {/* Left Side Panel (or Full Width on Mobile) */}
+      <div
+        className={`${
+          isMobile
+            ? "w-full"
+            : "w-[41%] h-screen overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-transparent [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-thumb]:border-2 [&::-webkit-scrollbar-thumb]:border-solid [&::-webkit-scrollbar-thumb]:border-transparent [&::-webkit-scrollbar-thumb]:bg-clip-padding  hover:[&::-webkit-scrollbar-thumb]:bg-gray-400 active:[&::-webkit-scrollbar-thumb]:bg-gray-500 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500 dark:hover:[&::-webkit-scrollbar-thumb]:bg-neutral-400 dark:active:[&::-webkit-scrollbar-thumb]:bg-neutral-300 "
+        } p-4`}
+      >
+        {/* First Accordion */}
+        <Accordion type="single" collapsible defaultValue="text-inputs">
+          <AccordionItem value="text-inputs">
+            <AccordionTrigger>Teams & match input</AccordionTrigger>
+            <AccordionContent className="">
+              <Label className="font-base text-gray-400">
+                Add registered teams and matches played here.
+              </Label>
+              <div className="pt-4">
+                <div className="mb-4">
+                  {/** Text Area */}
+                  <div className="grid w-full gap-1.5">
+                    <Label htmlFor="message-2">Teams</Label>
+                    <Textarea
+                      placeholder="Type your message here."
+                      id="message-2"
+                    />
+                  </div>
+                  <section
+                    id="button-container"
+                    className="flex justify-between mt-4"
+                  >
+                    <div className="w-[80%]">
+                      <Button className="w-full">Save</Button>
+                    </div>
+                    <div className="w-[20%] ml-2">
+                      <Button className="w-full" variant="outline">
+                        Clear
+                      </Button>
+                    </div>
+                  </section>
+                </div>
+                <div className="mb-4">
+                  {/** Text Area */}
+                  <div className="grid w-full gap-1.5">
+                    <Label htmlFor="message-2">Matches</Label>
+                    <Textarea
+                      placeholder="Type your message here."
+                      id="message-2"
+                    />
+                  </div>
+                  <section
+                    id="button-container"
+                    className="flex justify-between mt-4"
+                  >
+                    <div className="w-[80%]">
+                      <Button className="w-full">Save</Button>
+                    </div>
+                    <div className="w-[20%] ml-2">
+                      <Button className="w-full" variant="outline">
+                        Clear
+                      </Button>
+                    </div>
+                  </section>
+                </div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        {/* Second Accordion */}
+
+        <Accordion type="single" collapsible defaultValue="logging-history">
+          <AccordionItem value="logging-history">
+            <AccordionTrigger>Logs</AccordionTrigger>
+            <AccordionContent>
+              <div className="p-4">
+                <div className="border rounded p-2 mb-2">
+                  Log/History Item 1
+                </div>
+                <div className="border rounded p-2 mb-2">
+                  Log/History Item 2
+                </div>
+                <div className="border rounded p-2">Log/History Item 3</div>
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
-      <h1>Vite + React + TailwindCSS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* Right Side Panel (or Below on Mobile) */}
+      <div
+        className={`${isMobile ? "w-full" : "w-[55%]"} p-4`}
+        id="right-panel"
+      >
+        {/** Tabs */}
+        <Tabs defaultValue="teams" className="flex border-b flex-col">
+          <TabsList>
+            <TabsTrigger value="teams">Teams</TabsTrigger>
+            <TabsTrigger value="matches">Matches</TabsTrigger>
+            <TabsTrigger value="leaderboards">Leaderboards</TabsTrigger>
+          </TabsList>
+          {/** Tabs content */}
+          <TabsContent value="teams">
+            <TableDemo />
+          </TabsContent>
+          <TabsContent value="matches">matches table</TabsContent>
+          <TabsContent value="leaderboards">leaderboards table</TabsContent>
+        </Tabs>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Error: {(error as Error).message}</p>}
-      {data && <b className="pb-4">{data.message}</b>}
-    </>
+    </div>
   );
 }
 
